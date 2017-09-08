c5374faa1e144e7b4c7d5ccb59797e81dfcca509
nailuoluo <nailuoluo@gmail.com>
2017-09-08 23:20:48 +0800

    add moshi lib

---
+++ app/src/main/java/com/momiji/kotlin/ayaaya/module/WeiboApi.kt
@@ -0,0 +1,11 @@
+package com.momiji.kotlin.ayaaya.module
+
+import com.momiji.kotlin.ayaaya.ui.MyPreferences
+import retrofit2.http.GET
+
+/**
+ * Created by Momiji on 2017/9/8.
+ */
+interface WeiboApi {
+
+}
